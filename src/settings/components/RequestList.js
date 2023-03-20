f5fb898df4ba0b8e43e61d3646f5044366418305
petaryoast <86779744+petaryoast@users.noreply.github.com>
2023-03-20 10:03:57 +0100

    :fire:

--- src/settings/components/RequestList.js
+++ src/settings/components/RequestList.js
@@ -1,3 +1,5 @@
+
+
 export default function RequestList( props ) {
 	if ( ! props.requests ) {
 		return null;
@@ -9,10 +11,8 @@ export default function RequestList( props ) {
 			summary.totalResponseSize += event.response_size;
 			summary.totalRequestSize += event.request_size;
 			summary.averageRuntime = summary.totalRuntime / summary.total;
-			summary.averageResponseSize =
-				summary.totalResponseSize / summary.total;
-			summary.averageRequestSize =
-				summary.totalRequestSize / summary.total;
+			summary.averageResponseSize = summary.totalResponseSize / summary.total;
+			summary.averageRequestSize = summary.totalRequestSize / summary.total;
 			return summary;
 		}, {
 			total: 0,
@@ -25,30 +25,26 @@ export default function RequestList( props ) {
 		} );
 
 		return (
-			<tr>
-				<td>
-					{ request.post_title
-						.replace( 'GET_', 'GET ' )
-						.replace( 'POST_', 'POST ' ) }
-				</td>
-				<td>{ aggregate.total }</td>
-				<td>{ parseFloat( aggregate.averageRuntime ).toFixed( 2 ) } s</td>
-				<td>{ parseFloat( aggregate.totalRequestSize / 1000 ).toFixed( 2 ) } kB</td>
-				<td>{ parseFloat( aggregate.totalResponseSize / 1000 ).toFixed( 2 ) } kB</td>
-			</tr>
+			<div className="eco-mode__table-body-row">
+				<div className="eco-mode__table-column url">{ request.post_title.replace( 'GET_', 'GET - ' ).replace( 'POST_', 'POST - ' ) }</div>
+				<div className="eco-mode__table-column total">{ aggregate.total }</div>
+				<div className="eco-mode__table-column avg-runtime">{ parseFloat( aggregate.averageRuntime ).toFixed( 2 ) } s</div>
+				<div className="eco-mode__table-column req-size">{ parseFloat( aggregate.totalRequestSize / 1000 ).toFixed( 2 ) } kB</div>
+				<div className="eco-mode__table-column res-size">{ parseFloat( aggregate.totalResponseSize / 1000 ).toFixed( 2 ) } kB</div>
+			</div>
 		);
 	} );
 
-	return <table>
-		<thead>
-			<th>URL</th>
-			<th>Total</th>
-			<th>Average runtime</th>
-			<th>Total request size</th>
-			<th>Total response size</th>
-		</thead>
-		<tbody>
+	return <div className="eco-mode__table">
+		<div className="eco-mode__table-header">
+			<div className="eco-mode__table-column url">URL</div>
+			<div className="eco-mode__table-column total">Total</div>
+			<div className="eco-mode__table-column avg-runtime">Average runtime</div>
+			<div className="eco-mode__table-column req-size">Total request size</div>
+			<div className="eco-mode__table-column res-size">Total response size</div>
+		</div>
+		<div className="eco-mode__table-body">
 			{ rows }
-		</tbody>
-	</table>
+		</div>
+	</div>
 }
