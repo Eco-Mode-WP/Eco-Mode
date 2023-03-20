b888439d13fa8ed06c4451f087c5abc08437be74
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-03-20 10:00:18 +0100

    Style panel body

--- build/settings/settings.asset.php
+++ build/settings/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3bd4b1dc61f9c02aacf1');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2448d722bfb4b81524f5');
