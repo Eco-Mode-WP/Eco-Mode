eb69de553d3350a618313c8a3eb9d866758177d2
Zsolt Revay <zsolt.revay.g@gmail.com>
2023-03-20 09:35:11 +0100

    Fix tabs / Update filter UI / Update text

--- build/settings/settings.asset.php
+++ build/settings/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '39154be6f3858781e1b2');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6c5c62219123f6580655');
