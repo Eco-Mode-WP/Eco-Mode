f41a45e09a4b90e594670df90c1c5c64a2c10357
Thierry Muller <thierrymuller@google.com>
2023-03-20 10:45:00 +0100

    Add autoloader to uninstaller

--- uninstall.php
+++ uninstall.php
@@ -7,6 +7,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+$ecomode_blog_autoloader = __DIR__ . '/vendor/autoload.php';
+if ( is_readable( $ecomode_blog_autoloader ) ) {
+	require_once $ecomode_blog_autoloader;
+}
+
 $options = Alter_Schedule::get_options();
 
 if ( empty( $options ) ) {
