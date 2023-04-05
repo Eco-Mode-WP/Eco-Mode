0396a0921831f129e57fd193ec92f308e2f28d51
Hans-Christiaan <hans-christiaan@yoast.com>
2023-04-05 13:05:33 +0200

    Added doc blocks to first unit test. Adapted namespace of unit test.

--- tests/includes/ThrottledRequestTest.php
+++ tests/includes/ThrottledRequestTest.php
@@ -1,12 +1,24 @@
 <?php
 
-namespace includes;
+namespace EcoMode\EcoModeWP\Tests\Includes;
 
 use EcoMode\EcoModeWP\ThrottledRequest;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Tests for the Throttled Request class.
+ *
+ * @coversDefaultClass ::ThrottledRequest
+ */
 final class ThrottledRequestTest extends TestCase
 {
+
+	/**
+	 * Tests that creating a throttled request without setting a method parameter
+	 * defaults the method parameter to `GET`.
+	 *
+	 * @return void
+	 */
 	public function test_has_GET_as_default_value(): void {
 		$url = "https://example.org";
 		$max_frequency_in_seconds = 10;
