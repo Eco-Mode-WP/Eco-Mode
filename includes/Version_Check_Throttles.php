a23dfae0f0eefbf31d797cadad02094d24cb70a2
Vraja Das <vraja.pro@gmail.com>
2023-04-15 10:05:55 +0200

    Cs fix for Version_Check_Throttles class

--- includes/Version_Check_Throttles.php
+++ includes/Version_Check_Throttles.php
@@ -1,31 +1,47 @@
 <?php
+/**
+ * Handles throttles for wp_version_check.
+ *
+ * @package EcoMode
+ */
+
 namespace EcoMode\EcoModeWP;
 
 /**
  * Handles throttles for wp_version_check.
  */
 class Version_Check_Throttles {
-    const FILE_MODS_THROTTLER  = 'file_mods';
-    const WP_VERSION_THROTTLER = 'wp_version';
-    const REQUEST_URL      = 'https://api.wordpress.org/core/version-check/';
-    const SCHEDULED_ACTION = 'wp_version_check';
+	const FILE_MODS_THROTTLER  = 'file_mods';
+	const WP_VERSION_THROTTLER = 'wp_version';
+	const REQUEST_URL          = 'https://api.wordpress.org/core/version-check/';
+	const SCHEDULED_ACTION     = 'wp_version_check';
 
-    /**
-     * Initializer for throttles of wp_version_check.
-     */
-    public static function init(): void {
-        
-        Alter_Schedule::register_alteration( self::FILE_MODS_THROTTLER, ! self::is_file_mod_allowed(), self::SCHEDULED_ACTION, false, self::REQUEST_URL );
+	/**
+	 * Initializer for throttles of wp_version_check.
+	 */
+	public static function init(): void {
+
+		Alter_Schedule::register_alteration( self::FILE_MODS_THROTTLER, ! self::is_file_mod_allowed(), self::SCHEDULED_ACTION, false, self::REQUEST_URL );
 
 		Alter_Schedule::register_alteration( self::WP_VERSION_THROTTLER, self::is_wp_outdated(), self::SCHEDULED_ACTION, 'weekly', self::REQUEST_URL );
-    }
+	}
 
-    private static function is_file_mod_allowed() {
-        return ! defined( 'DISALLOW_FILE_MODS' ) || ! DISALLOW_FILE_MODS;
-    }
+	/**
+	 * Checks if file modifications are allowed.
+	 *
+	 * @return bool
+	 */
+	private static function is_file_mod_allowed() {
+		return ! defined( 'DISALLOW_FILE_MODS' ) || ! DISALLOW_FILE_MODS;
+	}
 
+	/**
+	 * Checks if WordPress is outdated.
+	 *
+	 * @return bool
+	 */
 	private static function is_wp_outdated() {
-		// return false;
+		// Returns false.
 		$current = get_site_transient( 'update_core' );
 
 		if ( ! isset( $current->updates ) || ! is_array( $current->updates ) ) {
